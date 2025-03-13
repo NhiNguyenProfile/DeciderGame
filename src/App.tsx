e6f5108672c62cb0f57dbb8fa05d14f562fa737b
Nguyen Ngoc Y Nhi <ynhiworkplace308@gmail.com>
2025-03-13 23:30:57 +0700

    Chore/Nhi: Build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import SwipeGame from "./pages/GamePlay";
 
 function App() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    const playAudio = () => {
+      if (audioRef.current && !isPlaying) {
+        audioRef.current
+          .play()
+          .then(() => {
+            setIsPlaying(true);
+          })
+          .catch((e) => console.log("Autoplay bị chặn:", e));
+      }
+    };
+
+    document.addEventListener("click", playAudio);
+    return () => document.removeEventListener("click", playAudio);
+  }, [isPlaying]);
+
   return (
     <>
       <SwipeGame />
+      <audio ref={audioRef} loop>
+        <source src="/music/Retro_Groove.mp3" type="audio/mp3" />
+      </audio>
     </>
   );
 }
